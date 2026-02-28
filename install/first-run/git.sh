41f1b86c59c1fd770fa517a1aa9cdb1b9900e798
Joe Ferreira Scholtz <joefscholtz@gmail.com>
2026-02-27 21:39:57 -0300

    wip

---
+++ install/first-run/git.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Joe Ferreira Scholtz"
+git config --global user.email "joefscholtz@gmail.com"
+git config --global pull.rebase false #TODO: check if ff only is better as default
+git config --global core.autocrlf input
+git config --global core.editor "vim"
